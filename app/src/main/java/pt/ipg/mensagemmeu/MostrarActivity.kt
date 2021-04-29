0a6f133ba9037fa32bdf0451b7bc62460d16d51a
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-04-29 23:27:19 +0100

    Criação da atividade e melhoramemntos no layout

---
+++ app/src/main/java/pt/ipg/mensagemmeu/MostrarActivity.kt
@@ -0,0 +1,11 @@
+package pt.ipg.mensagemmeu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MostrarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar)
+    }
+}
