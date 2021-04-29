9feca2b7ec8aeca509fa691c85cde9f7e80472c1
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-04-30 00:09:52 +0100

    Intent completo, passagem da mensagem de cada campo completa

--- app/src/main/java/pt/ipg/mensagemmeu/MainActivity.kt
+++ app/src/main/java/pt/ipg/mensagemmeu/MainActivity.kt
@@ -1,11 +1,51 @@
 package pt.ipg.mensagemmeu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        public val INFO_EXTRA_NOME :String = "NOME"
+        public val INFO_EXTRA_EMAIL :String = "EMAIL"
+        public val INFO_EXTRA_TELE :String = "TELE"
+        public val INFO_EXTRA_IDADE :String = "IDADE"
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun enviaMensagem(view: View){
+        //obter a mensagem
+        val textInputEditNome = findViewById<EditText>(R.id.TextInputEditNome)
+        val nome = textInputEditNome.text.toString()
+
+
+        val textInputEditEmail = findViewById<EditText>(R.id.TextInputEditEmail)
+        val email = textInputEditEmail.text.toString()
+
+
+        val textInputEditTele = findViewById<EditText>(R.id.TextInputEditTele)
+        val tele = textInputEditTele.text.toString()
+
+
+        val textInputEditIdade = findViewById<EditText>(R.id.TextInputEditIdade)
+        val idade = textInputEditIdade.text.toString()
+
+        //passar entre atividades
+        val intent = Intent(this,MostrarActivity::class.java)
+        intent.putExtra(INFO_EXTRA_NOME,nome)
+        intent.putExtra(INFO_EXTRA_EMAIL,email)
+        intent.putExtra(INFO_EXTRA_TELE,tele)
+        intent.putExtra(INFO_EXTRA_IDADE,idade)
+
+        startActivity(intent)
     }
 }
--- app/src/main/java/pt/ipg/mensagemmeu/MostrarActivity.kt
+++ app/src/main/java/pt/ipg/mensagemmeu/MostrarActivity.kt
@@ -2,10 +2,25 @@ package pt.ipg.mensagemmeu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MostrarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar)
+
+        val nome = intent.getStringExtra(MainActivity.INFO_EXTRA_NOME)
+        val textViewNome = findViewById<TextView>(R.id.textViewNome)
+        textViewNome.setText(nome)
+        val email = intent.getStringExtra(MainActivity.INFO_EXTRA_EMAIL)
+        val textViewEmail = findViewById<TextView>(R.id.textViewEmail)
+        textViewEmail.setText(email)
+        val tele = intent.getStringExtra(MainActivity.INFO_EXTRA_TELE)
+        val textViewTele = findViewById<TextView>(R.id.textViewTele)
+        textViewTele.setText(tele)
+        val idade = intent.getStringExtra(MainActivity.INFO_EXTRA_IDADE)
+        val textViewIdade = findViewById<TextView>(R.id.textViewIdade)
+        textViewIdade.setText(idade)
     }
+
 }
